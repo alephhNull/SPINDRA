eb9d69a1d1ed02ffaf74fca0c8bbbe544946bfcd
marzieh <marzieh@gmail.com>
2025-05-15 17:11:15 +0330

    add umap

--- preprocess_tumor.py
+++ preprocess_tumor.py
@@ -5,13 +5,17 @@ def preprocess_tumor(file_path):
     # 1. Load the data (keep)
     adata = sc.read_h5ad(file_path)
     print("Shape of Oringial Single-Cell tumor adata:", adata.shape)
-
+    
     # 2. Quality Control (keep)
     adata.var['mt'] = adata.var_names.str.startswith('MT')
     sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
     # sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'], jitter=0.4, multi_panel=True)
 
-    adata = adata[(adata.obs['orig.ident'] == 'Pre_P018_t') | (adata.obs['orig.ident'] == 'Pre_P020_t'),:]
+    adata = adata[(adata.obs['orig.ident'] == 'Pre_P023_t') | 
+              (adata.obs['orig.ident'] == 'Pre_P018_t')| 
+              (adata.obs['orig.ident'] == 'Pre_P025_t')| 
+              (adata.obs['orig.ident'] == 'Pre_P022_t')| 
+              (adata.obs['orig.ident'] == 'Pre_P020_t'),:]
 
     # Apply filters (keep)
     min_genes, max_genes, max_pct_mt = 200, 7000, 5
@@ -23,7 +27,18 @@ def preprocess_tumor(file_path):
         adata.raw.var.drop("_index", axis=1, inplace=True)
 
     print('Single Cell Tumor adata final shape:', adata.shape)
-    
+    sc.tl.pca(adata)
+
+    sc.pp.neighbors(adata)
+
+    sc.tl.umap(adata)
+
+    print(adata.obs['condition'])
+
+    sc.pl.umap(adata, color="orig.ident" , save= 'orig.iden_tumorsc.png')
+    sc.pl.umap(adata, color="condition" , save= 'condition_tumorsc.png')
+
+
     file_name = file_path.split('/')[-1]
     new_file_path = f'preprocessed/sc-tumor/{file_name}'
 
